6b4365b4800e35602fe9bd5174c3fa8ce578641e
Florencia Rosenfeld <hola@floristeady.com>
2021-03-24 13:43:33 -0600

    contact form php

--- js/main.js
+++ js/main.js
@@ -40,6 +40,77 @@ jQuery(function($){
 			return false;
 		});
     }
+    
+    function form() {
+	    var form = $('#form'),
+	      email = $('#email'),
+	      name = $('#name'),
+	      phone = $('#phone'),
+	      info = $('#info'),
+	      message = $('#message'),
+	      loader = $('#loader'),
+	      subject = $('#subject'),
+	      submit = $("#submit");
+	      
+	      form.on('input', '#email, #subject, #phone, #message', function() {
+		    $(this).css('border-color', '');
+		    info.html('').slideUp();
+		  });
+	      
+	      submit.on('click', function(e) {
+		    e.preventDefault();
+		    if(validate()) {
+		      $.ajax({
+		        type: "POST",
+		        url: "mailer.php",
+		        data: form.serialize(),
+		        dataType: "json"
+		      }).done(function(data) {
+		        if(data.success) {
+		          email.val('');
+		          phone.val('');
+		          subject.val('');
+		          message.val('');
+		          info.html('Message sent!').slideDown().siblings().slideUp();
+		        } else {
+		          loader.html('Could not send mail! Please try again later.').slideDown();
+		        }
+		      });
+		    } else {
+			    loader.html('Enter a valid information').slideDown().siblings().slideUp();
+		    }
+		  });
+    }
+    
+    function validate() {
+	    
+	     var email = $('#email'),
+	      subject = $('#subject'),
+	      phone = $('#phone'),
+	      message = $('#message');
+	      
+	    var valid = true;
+	    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	    
+	    if(!regex.test(email.val())) {
+	      email.css('border-color', 'red');
+	      valid = false;
+	    }
+	    if($.trim(subject.val()) === "") {
+	      subject.css('border-color', 'red');
+	      valid = false;
+	    }
+	    if($.trim(phone.val()) === "") {
+	      phone.css('border-color', 'red');
+	      valid = false;
+	    }
+	    if($.trim(message.val()) === "") {
+	      message.css('border-color', 'red');
+	      valid = false;
+	    }
+	    
+	    return valid;
+	}
 	
 	
 	/************************* 
@@ -48,6 +119,7 @@ jQuery(function($){
 
 	$(document).ready(function(){
 		tabsCarousel();
+		form();
 		
    	});
 
---
+++ mailer.php
@@ -0,0 +1,23 @@
+<?php
+
+if($_POST) {
+  $to = "frosenfeld@gmail.com"; // your mail here
+  $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+  $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
+  $phone = filter_var($_POST["phone"], FILTER_SANITIZE_STRING);
+  $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+  $body = "Website Contact Form\nName: $subject\nMessage: $message\nEmail: $email\nPhone: $phone";
+  
+  //mail headers are mandatory for sending email
+  $headers = 'From: ' .$email . "\r\n". 
+  'Reply-To: ' . $email. "\r\n" . 
+  'X-Mailer: PHP/' . phpversion();
+
+  if(@mail($to, $subject, $body, $headers)) {
+    $output = json_encode(array('success' => true));
+    die($output);
+  } else {
+    $output = json_encode(array('success' => false));
+    die($output);
+  }
+}
